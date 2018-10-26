baa233881c9f616f1187754c51b7fca153e13c09
Bane Sullivan <banesullivan@gmail.com>
2018-10-26 10:04:00 -0600

    Update sample script

--- scripts/sample_script.py
+++ scripts/sample_script.py
@@ -1,17 +1,21 @@
 #! pvpython
 
 # import the simple module from the paraview
-from paraview.simple import *
 from os import listdir
 from os.path import isfile, join
 from pvmacros import *
 
+import sys
+from paraview.simple import LoadPlugin
+sys.path.append('/Users/bane/anaconda3/envs/paraview/lib/python2.7/site-packages/')
+LoadPlugin('/Users/bane/Documents/OpenGeoVis/Software/PVGeo/PVPlugins/PVGeo_All.py')
+from paraview.simple import *
 
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
-# create a new 'Cone'
-cone1 = Cone()
+# create a new GlobeSource from PVGeo
+cone1 = GlobeSource()
 
 # get active view
 renderView1 = GetActiveViewOrCreate('RenderView')
@@ -29,5 +33,5 @@ renderView1.Update()
 
 #### uncomment the following to render all views and export
 RenderAllViews()
-export.exportVTKjs(FileName='test')
+export.exportVTKjs(FileName='test-pvpython')
 # alternatively, if you want to write images, you can use SaveScreenshot(...).
